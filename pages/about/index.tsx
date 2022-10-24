e392acf23c54a5613e30c9842f1d3b8f3d44d11b
Duy Pham <duypham9895@gmail.com>
2022-10-24 14:59:57 +0700

    Fix CSS

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -11,13 +11,10 @@ import "slick-carousel/slick/slick.css";
 import format from "date-fns/format";
 
 import HeadTitle from "../../components/UI/HeadTitle";
-import { IExperience } from "../../types";
 
-type Props = {
-  listExperience: IExperience[];
-};
+type Props = {};
 
-const About = ({ listExperience }: Props) => {
+const About = (props: Props) => {
   const formattedBirth = format(new Date("1999-10-18"), "MMM dd,yyyy");
   return (
     <>
@@ -25,11 +22,11 @@ const About = ({ listExperience }: Props) => {
       {/* End pageTitle */}
 
       <section>
-        <div className="container lg:rounded-2xl bg-white dark:bg-[#111111] px-4 sm:px-5 md:px-10 lg:px-20">
+        <div className="container lg:rounded-2xl bg-white dark:bg-[#151515] px-4 sm:px-5 md:px-10 lg:px-20">
           <div data-aos="fade">
             <div className="py-12">
               {/* Page Title */}
-              <h2 className="after-effect after:left-52 mt-12 lg:mt-0 ">
+              <h2 className="after-effect after:left-52 mt-12 lg:mt-0">
                 About Me
               </h2>
               <div className="grid grid-cols-12 md:gap-10 pt-4 md:pt-[40px] items-center">
@@ -131,63 +128,5 @@ const About = ({ listExperience }: Props) => {
     </>
   );
 };
-
-export async function getStaticProps() {
-  const listExperience: IExperience[] = [
-    {
-      id: "1",
-      icon: "/images/icons/icon-1.svg",
-      title: "Ui/Ux Design",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#D566FF",
-      bg: "#FCF4FF",
-    },
-    {
-      id: "2",
-      icon: "/images/icons/icon-2.svg",
-      title: "App Development",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#DDA10C",
-      bg: "#FEFAF0",
-    },
-    {
-      id: "3",
-      icon: "/images/icons/icon-3.svg",
-      title: "Photography",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#8774FF",
-      bg: "#FCF4FF",
-    },
-    {
-      id: "4",
-      icon: "/images/icons/icon-4.svg",
-      title: "Photography",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#FF6080",
-      bg: "#FFF4F4",
-    },
-    {
-      id: "5",
-      icon: "/images/icons/icon-5.svg",
-      title: "Managment",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#FF75D8",
-      bg: "#FFF0F8",
-    },
-    {
-      id: "6",
-      icon: "/images/icons/icon-6.svg",
-      title: "Web Development",
-      des: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam euismod volutpat.",
-      color: "#269FFF",
-      bg: "#F3FAFF",
-    },
-  ];
-  return {
-    props: {
-      listExperience,
-    },
-  };
-}
 
 export default About;
