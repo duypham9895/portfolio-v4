0159979994fdee7700b68afe9f6b38e76f26ae92
Duy Pham <duypham9895@gmail.com>
2022-10-24 22:55:44 +0700

    Add effect fade for every page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
-import "../styles/globals.css";
+import AOS from "aos";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+
+import "aos/dist/aos.css";
+import "../styles/globals.css";
+
 import PortfolioProvider from "../hooks/PortfolioProvider";
 import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+    AOS.refresh();
+  }, []);
   return (
     <PortfolioProvider>
       <Layout>
