7331773ea8a7f9c74ddfea8e5a2f6547471ef288
Duy Pham <duypham9895@gmail.com>
2022-10-24 15:09:37 +0700

    Fix CSS

--- components/layout/DetailContact.tsx
+++ components/layout/DetailContact.tsx
@@ -1,7 +1,10 @@
-import React, { useContext } from "react";
-import { FcGlobe, FcPhone, FcSms } from "react-icons/fc";
+import React from "react";
+import {
+  FaEnvelopeOpenText,
+  FaMapMarkerAlt,
+  FaMobileAlt,
+} from "react-icons/fa";
 
-import PortfolioContext from "../../hooks/portfolio-context";
 import { IContact } from "../../types";
 
 type Props = {
@@ -9,14 +12,13 @@ type Props = {
 };
 
 const DetailContact = ({ contact: { phone, email, address } }: Props) => {
-  const { defaultTheme } = useContext(PortfolioContext);
   return (
     <div className="w-full lg:w-[50%] xl:w-[40%] space-y-6">
       {/* Begin Phone */}
       <div className="flex flex-row flex-nowrap dark:bg-transparent p-[30px] bg-[#FFE5E5] dark:bg-[#212425] border-[#A6A6A6] gap-2 rounded-xl ">
         <span className="w-8 mt-1">
-          <span className="text-4xl dark:text-white">
-            <FcPhone />
+          <span className="text-4xl text-oriange ">
+            <FaMobileAlt />
           </span>
         </span>
         <div className="space-y-2">
@@ -29,8 +31,8 @@ const DetailContact = ({ contact: { phone, email, address } }: Props) => {
       {/* Begin Email */}
       <div className="flex flex-row flex-nowrap dark:bg-transparent p-[30px] bg-[#EEFBFF] dark:bg-[#212425] border-[#A6A6A6] gap-2 rounded-xl ">
         <span className="w-8 mt-1">
-          <span className="text-4xl dark:text-white">
-            <FcSms />
+          <span className="text-4xl text-green">
+            <FaEnvelopeOpenText />
           </span>
         </span>
         <div className="space-y-2">
@@ -43,8 +45,8 @@ const DetailContact = ({ contact: { phone, email, address } }: Props) => {
       {/* Begin Address */}
       <div className="flex flex-row flex-nowrap dark:bg-transparent p-[30px] bg-[#FAEFFD] dark:bg-[#212425] border-[#A6A6A6] gap-2 rounded-xl ">
         <span className="w-8 mt-1">
-          <span className="text-4xl dark:text-white">
-            <FcGlobe />
+          <span className="text-4xl text-oriange-lite">
+            <FaMapMarkerAlt />
           </span>
         </span>
         <div className="space-y-2">
